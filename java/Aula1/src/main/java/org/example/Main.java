49381df06b12d8fa1621318580830d0494f04e9e
Murilo Besson <murilo.besson@outlook.com>
2026-03-05 13:50:56 -0300

    Finalizando conceitos basicos de java (bootcamp)

--- java/Aula1/src/main/java/org/example/Main.java
+++ java/Aula1/src/main/java/org/example/Main.java
@@ -2,27 +2,62 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String nome = "Murilo Toniol";
+//        String nome = "Murilo Toniol";
+//
+//        char primeiraLetra = nome.charAt(0);
+//        System.out.println(primeiraLetra);
+//
+//        String primeiroNome = nome.substring(0, 6);
+//        System.out.println(primeiroNome);
+//
+//        String ultimoNome = nome.substring(7);
+//        System.out.println(ultimoNome);
+//
+//        String nomeSubstituto = "Almeida";
+//        nome = nome.replace("Toniol", nomeSubstituto);
+//        System.out.println(nome);
+//
+//        String login = "murilo";
+//
+//        if (login.equals("murilo")) {
+//            System.out.println("logado");
+//        }else{
+//            System.out.println("login incorreto");
+//        }
+//        int x = 3;
+//
+//        switch(x){
+//            case 1:
+//                System.out.println("numero 1");
+//            case 2:
+//                System.out.println("numero 2");
+//            case 3:
+//                System.out.println("numero 3");
+//        }
 
-        char primeiraLetra = nome.charAt(0);
-        System.out.println(primeiraLetra);
+//        //for (variavel; comparacao; operacao)
+//        for (int i = 0; i <=10; i++) {
+//            System.out.println(i);
+//        }
 
-        String primeiroNome = nome.substring(0, 6);
-        System.out.println(primeiroNome);
 
-        String ultimoNome = nome.substring(7);
-        System.out.println(ultimoNome);
+        int matriz[][] = new int[3][5];
 
-        String nomeSubstituto = "Almeida";
-        nome = nome.replace("Toniol", nomeSubstituto);
-        System.out.println(nome);
-
-        String login = "murilo";
-
-        if (login.equals("murilo")) {
-            System.out.println("logado");
-        }else{
-            System.out.println("login incorreto");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j=0; j<matriz[i].length; j++) {
+                matriz[i][j] = i+j;
+            }
         }
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j=0; j<matriz[i].length; j++) {
+                System.out.print("["+matriz[i][j]+"]");
+            }
+            System.out.println();
+        }
+
+
+
+
     }
 }
