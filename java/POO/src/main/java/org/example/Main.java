c777b81818accb0c04673e8b2fb6fa1a9953e848
Murilo Besson <murilo.besson@outlook.com>
2026-03-06 17:21:59 -0300

    POO sla

--- java/POO/src/main/java/org/example/Main.java
+++ java/POO/src/main/java/org/example/Main.java
@@ -21,11 +21,11 @@ public class Main {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c)/2.0;
-        double areaX = Math.sqrt(p*(p-x.a)*(p-x.b)*(p-x.c));
-
-        p=(y.a + y.b + y.c)/2.0;
-        double areaY = Math.sqrt(p*(p-y.a)*(p-y.b)*(p-y.c));
+//        double p = (x.a + x.b + x.c)/2.0;
+        double areaX = x.calculateArea();
+//
+//        p=(y.a + y.b + y.c)/2.0;
+        double areaY = y.calculateArea();
 
         System.out.printf("Triangle X area: %.4f%n", areaX);
         System.out.printf("Triangle Y area: %.4f%n", areaY);
