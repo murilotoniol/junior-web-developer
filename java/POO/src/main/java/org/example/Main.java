8367e6b1de99edbe066b0232e26fa25d886d378d
Murilo Besson <murilo.besson@outlook.com>
2026-03-06 11:46:46 -0300

    Revendo POO JAVA (depois de 1 ano)

---
+++ java/POO/src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Triangle x, y;
+        x=new Triangle();
+        y=new Triangle();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite as 3 medidas do triangulo x:");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+
+        System.out.println("Digite as 3 medidas do triangulo y:");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double p = (x.a + x.b + x.c)/2.0;
+        double areaX = Math.sqrt(p*(p-x.a)*(p-x.b)*(p-x.c));
+
+        p=(y.a + y.b + y.c)/2.0;
+        double areaY = Math.sqrt(p*(p-y.a)*(p-y.b)*(p-y.c));
+
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+
+        if (areaX > areaY){
+            System.out.println("X area is bigger");
+        }else{
+            System.out.println("Y area is bigger");
+        }
+        sc.close();
+    }
+}
