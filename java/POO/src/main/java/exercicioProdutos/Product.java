8e3bb6983d599c512a7e3654bb7dbcf25298f529
Murilo Besson <murilo.besson@outlook.com>
2026-03-09 17:48:47 -0300

    Encapsulamento e modificadores de acesso (private, protected)

--- java/POO/src/main/java/exercicioProdutos/Product.java
+++ java/POO/src/main/java/exercicioProdutos/Product.java
@@ -1,9 +1,9 @@
 package exercicioProdutos;
 
 public class Product {
-    String name;
-    double price;
-    int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
     public Product(String name, double price, int quantity) {
         this.name = name;
@@ -11,9 +11,29 @@ public class Product {
         this.quantity = quantity;
     }
 
-    public Product (String name, double price){
+    public Product(String name, double price) {
         this.name = name;
         this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 
     public double totalValueInStock(){
