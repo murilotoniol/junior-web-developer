72f72b9b339e4770d7383aaae8728b297d25efd0
Murilo Besson <murilo.besson@outlook.com>
2026-03-10 16:57:43 -0300

    Boxing, unboxing, for each

--- java/POO/src/main/java/vetores/Main.java
+++ java/POO/src/main/java/vetores/Main.java
@@ -6,22 +6,42 @@ public class Main{
     public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
 
-    int n = sc.nextInt();
-    Product[] vect = new Product[n];
+//    int n = sc.nextInt();
+//    Product[] vect = new Product[n];
+//
+//    for (int i=0;i<vect.length;i++){
+//        sc.nextLine();
+//        String name = sc.nextLine();
+//        double price = sc.nextDouble();
+//        vect[i] = new Product(name, price);
+//    }
+//
+//    double sum= 0.0;
+//    for (int i=0;i<vect.length;i++){
+//        sum += vect[i].getPrice();
+//    }
+//    double avg = sum/vect.length;
+//    System.out.printf("Average price: %.2f%n", avg);
+//
+//    // vou usar esse arquivo para escrever o codigo de boxign e unboxing
+//    // boxing
+//    int x = 20;
+//    Object obj = x;
+//    System.out.println(obj);
+//    // unboxing
+//    int y = (int)obj;
+//    System.out.println(y);
 
-    for (int i=0;i<vect.length;i++){
-        sc.nextLine();
-        String name = sc.nextLine();
-        double price = sc.nextDouble();
-        vect[i] = new Product(name, price);
+    // vou usar aqui pra exemplo de for each (para cada)
+    String[] nomes = new String[] {"Murilo", "Giovani", "Joao"};
+
+    for (int i=0;i<nomes.length;i++){
+        System.out.println(nomes[i]);        // for percorrendo cada elemento do vetor nomes
     }
 
-    double sum= 0.0;
-    for (int i=0;i<vect.length;i++){
-        sum += vect[i].getPrice();
+    for (String nome : nomes){   // para cada objeto 'nome' do tipo String contido em 'nomes[]' faça:
+        System.out.println(nome);
     }
-    double avg = sum/vect.length;
-    System.out.printf("Average price: %.2f%n", avg);
 
     sc.close();
     }
