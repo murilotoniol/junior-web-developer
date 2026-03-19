80692c6e9bef4b61fc7e8650616e5b061672c5f3
Murilo Besson <murilo.besson@outlook.com>
2026-03-19 09:39:59 -0300

    Entendendo a classe application

---
+++ java/Estrutura-Spring/src/test/java/com/murilo/Estrutura/Spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilo.Estrutura.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
