f37262787b07052959278334598e8b673099a471
Murilo Besson <murilo.besson@outlook.com>
2026-03-30 17:44:44 -0300

    lendo com @value e utilizando classes de config properties. Finalizando o modulo

--- java/Estrutura-Spring/src/main/java/com/murilo/Estrutura/Spring/Application.java
+++ java/Estrutura-Spring/src/main/java/com/murilo/Estrutura/Spring/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class Application {
 
 	public static void main(String[] args) {
