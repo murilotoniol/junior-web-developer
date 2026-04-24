f1e5a626c5af7090b0b00f3880dbc2b03d0b296b
Murilo Besson <murilo.besson@outlook.com>
2026-04-23 21:15:50 -0300

    Consegui fazer tudooooo, posso dormir tranquilo (corrigi ate coisas q o professor tinha feito de errado)

--- bootcamp/demo/src/main/java/com/example/demo/web/CarController.java
+++ bootcamp/demo/src/main/java/com/example/demo/web/CarController.java
@@ -1,9 +1,41 @@
 package com.example.demo.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.model.Car;
+import com.example.demo.service.CarService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cars")
 public class CarController {
+
+    private final CarService carService;
+
+    public CarController(@RequestBody CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Car>> findAll() {
+        return ResponseEntity.ok(carService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Car> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(carService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Car> create(@RequestBody Car car){
+        return ResponseEntity.status(HttpStatus.CREATED).body(carService.create(car));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Car> deleteById(@PathVariable Long id) {
+        carService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
