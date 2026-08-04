debd95a881a2a55bc86d3c7f74e4f9e8bbb82a00
Murilo Besson <murilo.besson@outlook.com>
2026-08-04 15:14:36 -0300

    Adicionando exemplo de Map nos headers

--- java/POO/src/main/java/map/Program.java
+++ java/POO/src/main/java/map/Program.java
@@ -23,22 +23,31 @@ public class Program {
 //            System.out.println(key+ ": " + cookies.get(key));
 //        }
 
-        Map<Product, Double> stock = new HashMap<>();
+//        Map<Product, Double> stock = new HashMap<>();
+//
+//        Product p1 = new Product("TV", 900.00);
+//        Product p2 = new Product("Notebook", 1200.00);
+//        Product p3 = new Product("Tablet", 400.00);
+//
+//        stock.put(p1, 100000.00);
+//        stock.put(p2, 200000.00);
+//        stock.put(p3, 300000.00);
+//
+//        Product ps = new Product("TV", 900.00);
+//        System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
+//
+//        for (Product prod : stock.keySet()){ // faz a leitura com base no produto (nesse caso o produto é a chave)
+//            System.out.println(stock.get(prod));
+//        }
 
-        Product p1 = new Product("TV", 900.00);
-        Product p2 = new Product("Notebook", 1200.00);
-        Product p3 = new Product("Tablet", 400.00);
+        Map<String, String> headers = new HashMap<>();
 
-        stock.put(p1, 100000.00);
-        stock.put(p2, 200000.00);
-        stock.put(p3, 300000.00);
+        headers.put("Content-Type", "application/json");
+        headers.put("Authorization", "Bearer eyJhbGciOi...");
+        headers.put("Accept-Language", "pt-BR");
 
-        Product ps = new Product("TV", 900.00);
-        System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
-
-        for (Product prod : stock.keySet()){ // faz a leitura com base no produto (nesse caso o produto é a chave)
-            System.out.println(stock.get(prod));
-        }
+        String token = headers.get("Authorization");
+        System.out.println("Token retornado: " + token);
 
     }
 }
