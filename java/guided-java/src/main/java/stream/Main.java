77f916c0f54b144649aa91447d003d09555d8049
Murilo Besson <murilo.besson@outlook.com>
2026-08-12 17:11:01 -0300

    Streams - Pipeline

--- java/guided-java/src/main/java/stream/Main.java
+++ java/guided-java/src/main/java/stream/Main.java
@@ -2,6 +2,7 @@ package stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -19,6 +20,21 @@ public class Main {
 
         Stream<Long> fibonacci = Stream.iterate(new long[]{0L, 1L}, p -> new long[]{p[1], p[0] + p[1]}).map(p -> p[0]);
         System.out.println(Arrays.toString(fibonacci.limit(30).toArray()));
+
+        //============== PIPELINE ==============
+
+        List<Integer> list1 = Arrays.asList(3, 4, 5, 10, 7);
+        Stream<Integer> st4 = list1.stream().map(x -> x * 10);
+        System.out.println(Arrays.toString(st4.toArray()));
+
+        int sum = list.stream().reduce(0, (x,y) -> x + y);
+        System.out.println("Sum: " + sum);
+
+        List<Integer> newList = list.stream()
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * 10)
+                .collect(Collectors.toList());
+        System.out.println(Arrays.toString(newList.toArray()));
 
     }
 }
